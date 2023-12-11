de0ac091c0b67eb5b686495dd8e3b532130b8449
nathan-verdeyme <nathan.verdeyme@outlook.fr>
2023-12-11 15:34:52 +0100

    implementation de home

--- app/src/main/java/com/example/projectjeu/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/projectjeu/ui/home/HomeFragment.java
@@ -1,33 +1,76 @@
 package com.example.projectjeu.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.projectjeu.R;
 import com.example.projectjeu.databinding.FragmentHomeBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class HomeFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
-    private FragmentHomeBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        return root;
-    }
+public class HomeFragment extends AppCompatActivity {
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_home);
+
+        ArrayList<Car> listData = getListData();
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new CustomListAdapter(this, listData));
+
+        // When the user clicks on the ListItem
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Object o = listView.getItemAtPosition(position);
+                Car upload = (Car) o;
+                Intent intent = new Intent(MainActivity.this, EditVoitureActivity.class);
+                intent.putExtra("id", upload.getId());
+                intent.putExtra("marque", upload.getMarque());
+                intent.putExtra("modele", upload.getModele());
+                intent.putExtra("carburant", upload.getCarburant());
+                intent.putExtra("price", upload.getPrice());
+                startActivity(intent);
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditVoitureActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public ArrayList<Car> getListData(){
+        try{
+            ConnectionRest connectionRest = new ConnectionRest();
+            connectionRest.execute("GET");
+            String listJsonObjs = connectionRest.get();
+            if(listJsonObjs != null) {
+                return connectionRest.parse(listJsonObjs);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
