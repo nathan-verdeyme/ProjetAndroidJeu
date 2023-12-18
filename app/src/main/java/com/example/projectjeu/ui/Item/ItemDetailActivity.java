ffef1373d55ccee7d723ccf5dd8c2e937c054445
Nabil Kerkoub <nabil.kerkoub@esme.fr>
2023-12-18 10:59:03 +0100

    Ajout de la partie ITEM

---
+++ app/src/main/java/com/example/projectjeu/ui/Item/ItemDetailActivity.java
@@ -0,0 +1,61 @@
+package com.example.projectjeu.ui.Item;
+
+import android.content.Intent;
+import android.media.effect.Effect;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.projectjeu.R;
+
+import com.example.projectjeu.ui.Item.ItemListAdapter;
+import com.example.projectjeu.ui.Item.Item;
+import com.example.projectjeu.ui.Item.ItemActivity;
+
+public class ItemDetailActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_item_detail);
+
+        String name = getIntent().getStringExtra("name");
+        String avatar = getIntent().getStringExtra("avatar");
+        int quantite = getIntent().getIntExtra("quantite", 0);
+        String effet = getIntent().getStringExtra("effet");
+        String Description = getIntent().getStringExtra("description");
+
+        TextView nameTextView = findViewById(R.id.detailName);
+        TextView quantiteTextView = findViewById(R.id.detailQuantite);
+        TextView effetTextView = findViewById(R.id.Effect);
+        TextView descriptionTextView = findViewById(R.id.Description);
+
+        nameTextView.setText(name);
+        quantiteTextView.setText("Quantité : " + String.valueOf(quantite));
+        effetTextView.setText("Effet: " + String.valueOf(effet));
+        descriptionTextView.setText("Description : " + String.valueOf(Description));
+
+
+        int resId = getResources().getIdentifier(avatar, "drawable", getPackageName());
+        ImageView avatarImage = findViewById(R.id.detailAvatar);
+        if (resId != 0 && avatarImage != null) {
+            avatarImage.setImageResource(resId);
+        } else {
+            avatarImage.setImageResource(R.mipmap.ic_launcher);
+        }
+
+        Button Ok = findViewById(R.id.Ok);
+        Ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("resultName", name);
+                resultIntent.putExtra("resultAvatar", resId);
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+    }
+}
